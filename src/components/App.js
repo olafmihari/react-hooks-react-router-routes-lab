b73a29e37fad1ba386fa3ce97a72058b9e2199ea
Olaf Mihari <olafnjenga@gmail.com>
2023-04-28 03:39:07 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,17 @@ import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/directors" component={Directors} />
+        <Route path="/actors" component={Actors} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
